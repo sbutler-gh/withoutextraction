f1f9a5b58f47b6f80bd7c938be9c0e7a31cd3c5e
sbutler-gh <sam@ysli.be>
2022-02-02 16:01:32 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,92 @@
+<!-- <h1></h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<h1 style="text-align: center;">Income Without Extraction</h1>
+
+<!-- <h3 style="text-align: center;">No:</h3>
+
+<div class="no-list" style="text-align: center; display: flex; flex-wrap: wrap;  justify-content: space-between;">
+    <p>Industrial Farming / Ranching</p>
+    <p>Mining</p>
+    <p>Poaching</p>
+    <p>Deforestation</p>
+    <p>Financializing Nature</p>
+    <p>Carbon Offsets</p>
+    <p>Extractive Projects</p>
+</div> -->
+
+<h3 style="text-align: center;">There are ways to earn <span>better income</span>,<br>
+<span>in reciprocation with nature.</span></h3>
+
+<div class="alt-list" style="text-align: center; display: flex; flex-wrap: wrap; justify-content: space-between">
+    <div style="max-width: 300px;">
+        <h4>Local filmmaking / Media</h4>
+        <p>The better a place is cared for, the more people will want to see it.</p>
+        <p><a href="https://filmmakermagazine.com/113084-alex-pritz-the-territory-sundance-2022/">The Territory</a> and <a href="https://ifnotusthenwho.me/">INUTW</a> show that communities are producing film on the ground.</p>
+        <p>Digital content is generating billions every year.  Frontline and Global South communities are capable of producing the media set in their own places, and benefitting from legacies of their care themselves.</p>
+    </div>
+    <div style="max-width: 300px;">
+        <h4>Local science / research</h4>
+        <p>The better a place is cared for, the more cures, discoveries, and innovations it holds.</p>
+        <p>Pharmaceuticals have a history of <a href="https://en.wikipedia.org/wiki/Bioprospecting#Biopiracy">biopiracy</a> medicinal research and innovations in places of rich biodiversity — without sharing incomes with the communities themselves.</p>
+        <!-- <p>World-changing cures, science, and discoveries are waiting in our forests, mountains, rivers, and oceans.  Communities are capable of conducting this research and making these findings themselves, which exist because of thousands of years of their stewardship.</p> -->
+        <p>Communities are capable of conducting this research and making these findings themselves, which we owe to thousands of years of their stewardship.</p>
+
+    </div>
+    <div style="max-width: 300px;">
+        <h4>Regenerative Agroforestry</h4>
+        <p>The better a place is cared for, the more fruit it gives</p>
+        <p>Around the world, farmers earn greater incomes through regenerative agroecoforestry than industrial farming and deforestation.</p>
+        <p>Why kill the land, rivers, and forests, when we can receive more gifts by supporting them?</p>
+    </div>
+</div>
+
+<div style="text-align: center" class="contact-box">
+<p>Interested in ending extraction, and trying transitions like these?  Is there any way you can use support?  Any other reason you would like to speak?</p>
+
+<div class="contact-items">
+    <p>Signal / WhatsApp<br><strong>+1 703 489 1697</strong></p>
+    <p>Email<br><strong>sam@sambutler.us</strong></p>
+    <p><a href="">Calendar</a></p>
+</div>
+
+<p>Organizations and individuals, interested in providing funding or collaborative support for initiatives like these?  Send a message to the contacts above, or set a time to speak on the calendar (and please include details about your interests and context.)</p>
+</div>
+<style>
+    .no-list p {
+        width: 100px;
+    }
+
+    .alt-list > div {
+        max-width: 300px;
+    text-align: center;
+    display: block;
+    margin: auto;
+    margin-top: 0;
+    }
+
+    .alt-list > div > h4 {
+        text-align: center;
+    }
+
+    .alt-list > div > p {
+        text-align: center;
+    }
+
+    .contact-box {
+        background: #efefef;
+    max-width: 600px;
+    padding: 10px;
+    border-radius: 5px;
+    margin: auto;
+    }
+
+    /* .contact-items {
+        text-align: center;
+        margin: auto;
+        max-width: 300px;
+    }
+
+    .contact-items p {
+        text-align: left;
+    } */
+</style>
